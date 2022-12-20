e2dcbba2857983925dcd29a460500708075b5c75
Sayem <mirazussalehin@outlook.com>
2022-12-20 23:45:47 +0600

    initial commit

---
+++ components/NavBar.vue
@@ -0,0 +1,76 @@
+<template>
+  <nav>
+    <div class="flex justify-between items-center pt-6 pr-10">
+      <div class="w-64 px-6">
+        <img src="../assets/images/logo.png" alt="">
+      </div>
+      <div ref="menu" class="hidden md:flex justify-between space-x-4 pr-6">
+        <a href="" class="nav-item">Research field</a>
+        <a href="" class="nav-item">Lab facilities</a>
+        <a href="" class="nav-item">Publications</a>
+        <a href="" class="nav-item">Youth network</a>
+        <NavbarDropdown />
+        <a href="" class="nav-item">Partners</a>
+      </div>
+
+      <div class="flex md:hidden button">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-6 w-6 transition-all"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M4 6h16M4 12h16M4 18h16"
+          />
+        </svg>
+      </div>
+    </div>
+    <!--mobile menu-->
+    <div class="mobile-menu hidden md:hidden mr-2 bg-white">
+      <div class="flex flex-col gap-2">
+        <a href="" class="nav-item">Research field</a>
+        <a href="" class="nav-item">Research item</a>
+        <a href="" class="nav-item">Lab facilities</a>
+        <a href="" class="nav-item">Publications</a>
+        <a href="" class="nav-item">Youth network</a>
+        <a href="" class="nav-item">Partners</a>
+        <NavbarDropdown />
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  mounted () {
+    const button = document.querySelector('div.button')
+    const menu = document.querySelector('div.mobile-menu')
+    button.addEventListener('click', () => {
+      menu.classList.toggle('hidden')
+    })
+  }
+}
+</script>
+
+<style>
+.nav-item {
+  @apply px-6 py-2 hover:cursor-pointer hover:bg-green-700 hover:text-white rounded-xl font-bold transition duration-500;
+}
+.mobile-nav {
+  @apply px-6 py-2 bg-white;
+}
+a.nuxt-link-active {
+  @apply text-white font-bold;
+}
+.group:hover .group-hover\:block {
+  display: block !important;
+}
+li{
+  @apply list-none;
+}
+</style>
---
+++ components/NavbarDropdown.vue
@@ -0,0 +1,120 @@
+<template>
+  <li id="button_admin" class="relative nav-item group mb-1 md:mb-0">
+    <a
+      class="font-semibold whitespace-no-wrap"
+      href=""
+    >
+      <span class="firstlevel">Amministra</span>
+    </a>
+    <ul
+      class="
+        absolute
+        left-0
+        top-0
+        mt-10
+        p-2
+        rounded-lg
+        shadow-lg
+        bg-white
+        z-10
+        hidden
+        group-hover:block
+      "
+    >
+      <svg
+        class="
+          block
+          fill-current
+          text-white
+          w-4
+          h-4
+          absolute
+          left-0
+          top-0
+          ml-3
+          -mt-3
+          z-0
+        "
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+      >
+        <path
+          d="M10.29 3.86L1.82 18a2 2 0 0 0 1.71 3h16.94a2 2 0 0 0 1.71-3L13.71 3.86a2 2 0 0 0-3.42 0z"
+        />
+      </svg>
+      <li
+        class="
+         mx-1
+          px-4
+          py-2
+          whitespace-no-wrap
+          rounded-md
+          text-sm
+          md:text-base
+          text-black
+          hover:text-green-700 hover:bg-gray-50
+        "
+      >
+        <a
+          class="px-2 py-1"
+          href=""
+        >
+          <span class="">Funzioni ed opzioni</span>
+        </a>
+      </li>
+      <li
+        class="
+         mx-1
+          px-4
+          py-2
+          whitespace-no-wrap
+          rounded-md
+          text-sm
+          md:text-base
+          text-black
+          hover:text-green-700 hover:bg-gray-50
+        "
+      >
+        <a
+          class="px-2 py-1"
+          href=""
+        >
+          <span class="">Gestore Pacchetti</span>
+        </a>
+      </li>
+      <li
+        class="
+         mx-1
+          px-4
+          py-2
+          whitespace-no-wrap
+          rounded-md
+          text-sm
+          md:text-base
+          text-black
+          hover:text-green-700 hover:bg-gray-50
+        "
+      >
+        <a
+          class="px-2 py-1"
+          href=""
+        >
+          <span class="">Permessi</span>
+        </a>
+      </li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.group:hover .group-hover\:block {
+  display: block !important;
+}
+li {
+  @apply list-none;
+}
+</style>
